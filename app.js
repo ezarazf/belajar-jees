4711368813e57265416ede92139d4305371dba66
ezarazf <70148854+ezarazf@users.noreply.github.com>
2025-04-19 22:32:43 +0700

    Refactor app.js to enhance variable declarations and improve code structure

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const myName = "Ezar";
-console.log(myName);
-// const gak bisa diubah
+const myAge = 25;
+const myHobby = "Nonton";
 
-let age = 20;
-// let bisa diubah
-age = 21;
-console.log(age);
+let orang = "Alok"; 
+orang = "Kelly"; // bisa diubah
+
+// Object
+// const product = {
+//     name: "Laptop",
+//     price: 2000,
+//     isAvailable: true,
+// }
+
+// console.log(product.name); // Laptop
+// console.log(product.price); // 2000
+// console.log(product.isAvailable); // true
+// console.log(product); // { name: 'Laptop', price: 2000, isAvailable: true }
+
+// Challenge
+const person = {
+    name: "Ezar",
+    age: 25,
+    born: 2008,
+    isEmployed: true,
+};
+
+const text = person.name + " Lahir pada tahun " + person.born;
+console.log(text);
+
